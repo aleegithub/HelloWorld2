98418b01f23211730be359b56216a4f9e21b9a3f
aleegithub <aleejc36@yahoo.com>
2014-09-17 20:46:30 -0400

    Initial commit

---
+++ app/src/main/java/com/leesasianbistro/helloworld/MainActivity.java
@@ -0,0 +1,73 @@
+package com.leesasianbistro.helloworld;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+
+public class MainActivity extends Activity {
+
+    Button showMsgButton;
+    Button newbutton;
+    Switch sw1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showMsgButton = (Button) findViewById(R.id.bttnShwMsg);
+
+        showMsgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hello World!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        newbutton = (Button) findViewById(R.id.btNew);
+
+       /* newbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Test", Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+        sw1 = (Switch) findViewById(R.id.switch1);
+        sw1.setTextOff("English");
+        sw1.setTextOn("中文");
+        sw1.setChecked(false);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
